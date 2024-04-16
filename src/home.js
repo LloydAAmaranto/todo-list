a97fe8f2eb77dcac18f9e7e8c7ff0e3728149ae3
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-04-15 22:58:03 -0400

    fixed layout, added a functional textbox and button to display what was entered

--- src/home.js
+++ src/home.js
@@ -8,3 +8,15 @@ function testPrint(){
     flag = 1;
   }
 }
+
+document.getElementById('taskButtonAdd').addEventListener('click', newTask)
+
+function newTask(){
+  const inputTask = document.getElementById('taskInputAdd').value;
+
+  var li = document.createElement('li');
+  var t = document.createTextNode(inputTask);
+  li.appendChild(t);
+
+  document.getElementById('list').appendChild(li);
+}
