ec0f21b88618d0be552de969a88fcb28539a6de0
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-04-15 23:41:31 -0400

    added textarea and buttons

--- src/home.js
+++ src/home.js
@@ -1,12 +1,23 @@
 document.getElementById('taskButtonAdd').addEventListener('click', newTask)
 
+// function newTask(){
+//   const inputTask = document.getElementById('taskInputAdd').value;
+
+//   var li = document.createElement('li');
+//   var t = document.createTextNode(inputTask);
+//   li.appendChild(t);
+
+//   document.getElementById('list').appendChild(li);
+//   document.getElementById('taskInputAdd').value = '';
+// }
+
 function newTask(){
   const inputTask = document.getElementById('taskInputAdd').value;
 
-  var li = document.createElement('li');
+  var textarea = document.createElement('textarea');
   var t = document.createTextNode(inputTask);
-  li.appendChild(t);
+  textarea.appendChild(t);
 
-  document.getElementById('list').appendChild(li);
-  document.getElementById('taskInputAdd').value = '';
+  document.getElementById('task-list').appendChild(textarea);
+  document.getElementById('taskInputAdd').value='';
 }
