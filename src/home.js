485b94caccc4bf0deb5556bdb7676385682a970d
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-04-15 23:01:48 -0400

    deleted unnecessary code

--- src/home.js
+++ src/home.js
@@ -1,14 +1,3 @@
-var flag = 1;
-function testPrint(){
-  if(flag == 1){
-    document.getElementById("test").innerHTML = "WHAT!";
-    flag = 0;
-  }else{
-    document.getElementById("test").innerHTML = "TESTING WORKS!!";
-    flag = 1;
-  }
-}
-
 document.getElementById('taskButtonAdd').addEventListener('click', newTask)
 
 function newTask(){
