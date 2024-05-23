567ecf9e9368fb291741e2aa2b7acb0efe12a261
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-05-23 16:48:35 -0400

    creates textbox along with the three buttons

--- src/home.js
+++ src/home.js
@@ -1,21 +1,42 @@
 document.getElementById('taskButtonAdd').addEventListener('click', newTask)
 
-
+//creates the new textbox
 function newTask(){
+
+  //create a new container for the new task
+  var textboxDiv = document.createElement("div");
+  textboxDiv.classList.add("textboxDiv");
+
+  //creates a new textarea
   var textbox = document.createElement("textarea");
-  textbox.type = "text";
   textbox.classList.add("textbox");
 
+  //create a new container for the buttons
+  var buttonDiv = document.createElement("div");
+  buttonDiv.classList.add("buttonDiv");
+
+
+  //creates the the buttons
+  var buttonDone = document.createElement("button");
+  buttonDone.textContent = "Done";  
+  var buttonEdit = document.createElement("button");
+  buttonEdit.textContent = "Edit";
+  var buttonDelete = document.createElement("button");
+  buttonDelete.textContent = "Delete";
+
+  buttonDiv.appendChild(buttonDone);
+  buttonDiv.appendChild(buttonEdit);
+  buttonDiv.appendChild(buttonDelete);
+ 
+  textboxDiv.appendChild(textbox);
+  textboxDiv.appendChild(buttonDiv);
 
   var taskContainer = document.getElementById("taskContainer");
+
   var firstChild = taskContainer.firstChild;
 
   var br = document.createElement("br");
   taskContainer.insertBefore(br, firstChild);
-
-  taskContainer.insertBefore(textbox, firstChild);
-
-  // var button = document.createElement("button");
-  // taskContainer.insertBefore(button, button);
+  taskContainer.insertBefore(textboxDiv, firstChild);
 
 }
