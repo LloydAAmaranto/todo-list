6885d8cf43213a78f73fc1630ccea116c79a0619
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-06-12 21:17:47 +0800

    created the close button for login popup and added feature to close popup by pressing the login button again

--- src/login.js
+++ src/login.js
@@ -1,7 +1,19 @@
+toggleLogin = 0;
+
+//ability to close the login popup by clicking on the "login" button
 function openLogin() {
-  document.getElementById("login").style.display = "block";
+  if(!toggleLogin){
+    document.getElementById("login").style.display = "block";
+    toggleLogin = 1;
+  }
+  else{
+    document.getElementById("login").style.display = "none";
+    toggleLogin = 0;
+  }
 }
 
+//ability to close the login popup by clicking the "X" button
 function closeLogin() {
   document.getElementById("login").style.display = "none";
+  toggleLogin = 0;
 }
